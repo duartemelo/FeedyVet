10bab9ec1f14521188c83060de58e7243e6d9f43
duartemelo <duarteribeirodemelo@hotmail.com>
2020-01-22 17:47:37 +0000

    Commit

--- react-feedy/src/App.js
+++ react-feedy/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
+import logo from "./Images/Logo_2_1600.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = "";
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="navbar">
+          <div className="brand feedy-logo-div">
+            <img src={logo} alt="Logo" className="feedy-logo-img"></img>
+          </div>
+          <div className="form-inline">
+            <Link to="/login">
+              <button className="button login">Login</button>
+            </Link>
+            <Link to="/register">
+              <button className="button register">Registo</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-feedy/src/index.js
+++ react-feedy/src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Login from "./Login";
+import Register from "./Register";
+import NotFound from "./notfound";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
