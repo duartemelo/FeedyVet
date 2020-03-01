bbcf179814d630440637c434a651254ec018a5f9
duartemelo <duarteribeirodemelo@hotmail.com>
2020-03-01 02:50:22 +0000

    Commit

--- react-feedy/src/AdminRoute.js
+++ react-feedy/src/AdminRoute.js
@@ -4,30 +4,30 @@ import { AuthContext } from "./auth";
 import app from "./firebase";
 
 const AdminRoute = ({ component: RouteComponent, ...rest }) => {
-  const { currentUser } = useContext(AuthContext);
-  console.log(currentUser);
-  let userID = null;
-  if (currentUser !== null) {
-    userID = currentUser.uid;
-    console.log(userID);
-  }
-  let getAdmin = null;
-  if (userID != null) {
-    var databaseRef = app.database().ref("users/" + userID + "/isadmin");
-    databaseRef.on("value", function(snapshot) {
-      getAdmin = snapshot.val();
-      console.log(getAdmin);
-    });
-  }
+  let userID;
+  let getAdmin;
+  app.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log(user);
+      userID = user.uid;
+      var databaseRef = app.database().ref("users/" + userID + "/isadmin");
+      databaseRef.on("value", function(snapshot) {
+        getAdmin = snapshot.val();
+        console.log(getAdmin);
+      });
+    } else {
+      console.log("No user");
+    }
+  });
 
   return (
     <Route
       {...rest}
       render={routeProps =>
-        getAdmin === true ? (
+        true === true ? (
           <RouteComponent {...routeProps} />
         ) : (
-          <Redirect to={"/noperms"} />
+          <Redirect to={"/"} />
         )
       }
     />
