53916bb8e5cac3462d4caf0e4cbeb76c4504579b
duartemelo <duarteribeirodemelo@hotmail.com>
2020-03-02 10:56:53 +0000

    Commit

--- react-feedy/src/PrivateRoute.js
+++ react-feedy/src/PrivateRoute.js
@@ -1,20 +1,46 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect, Component } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { AuthContext } from "./auth";
+import app from "./firebase";
+import Loader from "react-loader-spinner";
+import "./Loader.css";
 
 const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
-  const { currentUser } = useContext(AuthContext);
-  return (
-    <Route
-      {...rest}
-      render={routeProps =>
-        !!currentUser ? (
-          <RouteComponent {...routeProps} />
-        ) : (
-          <Redirect to={"/"} />
-        )
+  /*const { currentUser } = useContext(AuthContext);*/
+  const [user, setUser] = useState(undefined);
+  const [redirect, setRedirect] = useState(false);
+
+  
+
+
+  useEffect(() => {
+    app.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser("nouser")
+        console.log("No user.");
       }
-    />
+    });
+  }, []);
+
+  
+
+  return (
+    <div>
+      {user === undefined ? (
+        <div class="loader-container">
+          <Loader type="TailSpin" color="#3680C1" width={100} height={100} />
+        </div>
+      ) : (
+        <Route
+          {...rest}
+          render={routeProps =>
+            user !== "nouser" ? <RouteComponent {...routeProps} /> : <Redirect to={"/"} />
+          }
+        />
+      )}
+    </div>
   );
 };
 
