e87afbb7d1e38d5a439fda024e6984a308e87c64
duartemelo <duarteribeirodemelo@hotmail.com>
2020-03-05 21:01:29 +0000

    Commit

--- react-feedy/src/Pages/Admin.js
+++ react-feedy/src/Pages/Admin.js
@@ -1,21 +1,43 @@
 import React, { Component } from "react";
 import "../styles/global/Main.css";
 import app from "../firebase";
+import TestAdminPage from "./TestAdminPage";
 
 class Admin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventsID: [],
+      eventsAnimal: [],
+      eventsDateTime: [],
+      eventsType: [],
+      eventsUserID: []
+    };
+  }
+
+  
+
+
   render() {
     const db = app.database();
-    var ref = db.ref("events");
-    ref.orderByChild("datetime").on("child_added", function(snapshot) {
-      console.log(
-        "The key is: " +
-          snapshot.key +
-          " and the userID is " +
-          snapshot.val().userID +
-          " and the type of event is " +
-          snapshot.val().type
-      );
-    });
+    const ref = db.ref("events");
+
+    const addEvent = () => {
+      ref.orderByChild("datetime").on("child_added", function(snapshot) {
+        console.log(
+          "The key is: " +
+            snapshot.key +
+            " and the userID is " +
+            snapshot.val().userID +
+            " and the type of event is " +
+            snapshot.val().type
+        );
+        
+      });
+    };
+
+    addEvent();
+
     return (
       <div>
         <h1>Admin Panel</h1>
@@ -23,6 +45,8 @@ class Admin extends Component {
       </div>
     );
   }
+
+  
 }
 
 export default Admin;
