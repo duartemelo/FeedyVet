1f82de4cb178ea3eaabeb6298c293e364978311e
duartemelo <duarteribeirodemelo@hotmail.com>
2020-04-12 17:26:49 +0100

    Time Selector
    Time selector, obtem eventos de hoje, passado e futuro.
    Com problema porque não dá reset ao state de cada vez que se carrega num botao do time selector.

--- react-feedy/src/Pages/Animal.js
+++ react-feedy/src/Pages/Animal.js
@@ -23,47 +23,125 @@ class Animal extends Component {
   }
 
   resetState() {
-    this.setState(prestate);
+    this.setState({
+      eventsID: [],
+      eventsAnimal: [],
+      eventsDateTime: [],
+      eventsType: [],
+      eventsUserID: [],
+      eventsUserName: [],
+    });
+  }
+
+  testResetState() {
+    alert("test");
   }
 
   componentDidMount() {
     let currentComponent = this;
     setTimeout(function () {
-      currentComponent.getEvents();
+      currentComponent.getEvents("today");
     }, 10);
   }
 
-  getEvents() {
-    this.resetState();
+  getEvents(timeSelected) {
     const db = app.database();
     const ref = db.ref("events");
     let currentComponent = this;
 
-    ref.orderByChild("datetime").on("child_added", function (snapshot) {
-      if (snapshot.val().userID === app.auth().currentUser.uid) {
-        var momentDate = moment(snapshot.val().datetime);
-        momentDate.toDate();
+    if (timeSelected === "past") {
+      ref.orderByChild("datetime").on("child_added", function (snapshot) {
+        if (snapshot.val().userID === app.auth().currentUser.uid) {
+          var eventDate = moment(snapshot.val().datetime);
+          eventDate.toDate();
+          var today = moment();
+          var todayFormated = today.format("DD-MM-YYYY");
 
-        currentComponent.setState({
-          eventsID: currentComponent.state.eventsID.concat(snapshot.key),
-          eventsAnimal: currentComponent.state.eventsAnimal.concat(
-            snapshot.val().animal
-          ),
-          eventsDateTime: currentComponent.state.eventsDateTime.concat(
-            momentDate.format("DD-MM-YYYY, H:mm")
-          ),
-          eventsType: currentComponent.state.eventsType.concat(
-            snapshot.val().type
-          ),
-          eventsUserID: currentComponent.state.eventsUserID.concat(
-            snapshot.val().userID
-          ),
-          eventsUserName: currentComponent.state.eventsUserName.concat(
-            snapshot.val().userName
-          ),
-        });
-      }
-    });
+          if (eventDate.format("DD-MM-YYYY") < todayFormated) {
+            currentComponent.setState({
+              eventsID: currentComponent.state.eventsID.concat(snapshot.key),
+              eventsAnimal: currentComponent.state.eventsAnimal.concat(
+                snapshot.val().animal
+              ),
+              eventsDateTime: currentComponent.state.eventsDateTime.concat(
+                eventDate.format("DD-MM-YYYY, H:mm")
+              ),
+              eventsType: currentComponent.state.eventsType.concat(
+                snapshot.val().type
+              ),
+              eventsUserID: currentComponent.state.eventsUserID.concat(
+                snapshot.val().userID
+              ),
+              eventsUserName: currentComponent.state.eventsUserName.concat(
+                snapshot.val().userName
+              ),
+            });
+          }
+        }
+      });
+    }
+    if (timeSelected === "today") {
+      ref.orderByChild("datetime").on("child_added", function (snapshot) {
+        if (snapshot.val().userID === app.auth().currentUser.uid) {
+          var eventDate = moment(snapshot.val().datetime);
+          eventDate.toDate();
+          var today = moment();
+          var todayFormated = today.format("DD-MM-YYYY");
+
+          if (eventDate.format("DD-MM-YYYY") === todayFormated) {
+            currentComponent.setState({
+              eventsID: currentComponent.state.eventsID.concat(snapshot.key),
+              eventsAnimal: currentComponent.state.eventsAnimal.concat(
+                snapshot.val().animal
+              ),
+              eventsDateTime: currentComponent.state.eventsDateTime.concat(
+                eventDate.format("DD-MM-YYYY, H:mm")
+              ),
+              eventsType: currentComponent.state.eventsType.concat(
+                snapshot.val().type
+              ),
+              eventsUserID: currentComponent.state.eventsUserID.concat(
+                snapshot.val().userID
+              ),
+              eventsUserName: currentComponent.state.eventsUserName.concat(
+                snapshot.val().userName
+              ),
+            });
+          }
+        }
+      });
+    }
+    if (timeSelected === "future") {
+      ref.orderByChild("datetime").on("child_added", function (snapshot) {
+        if (snapshot.val().userID === app.auth().currentUser.uid) {
+          var eventDate = moment(snapshot.val().datetime);
+          eventDate.toDate();
+          var today = moment();
+          var todayFormated = today.format("DD-MM-YYYY");
+
+          if (eventDate.format("DD-MM-YYYY") > todayFormated) {
+            currentComponent.setState({
+              eventsID: currentComponent.state.eventsID.concat(snapshot.key),
+              eventsAnimal: currentComponent.state.eventsAnimal.concat(
+                snapshot.val().animal
+              ),
+              eventsDateTime: currentComponent.state.eventsDateTime.concat(
+                eventDate.format("DD-MM-YYYY, H:mm")
+              ),
+              eventsType: currentComponent.state.eventsType.concat(
+                snapshot.val().type
+              ),
+              eventsUserID: currentComponent.state.eventsUserID.concat(
+                snapshot.val().userID
+              ),
+              eventsUserName: currentComponent.state.eventsUserName.concat(
+                snapshot.val().userName
+              ),
+            });
+          }
+        }
+      });
+    }
   }
 
   render() {
@@ -77,8 +155,18 @@ class Animal extends Component {
           >
             Passado
           </button>
-          <button className={this.state.timeSelectTodayClasses}>Hoje</button>
-          <button className={this.state.timeSelectFutureClasses}>Futuro</button>
+          <button
+            className={this.state.timeSelectTodayClasses}
+            onClick={this.todayClick}
+          >
+            Hoje
+          </button>
+          <button
+            className={this.state.timeSelectFutureClasses}
+            onClick={this.futureClick}
+          >
+            Futuro
+          </button>
         </div>
         <div>
           {this.state.eventsID.map((id, index) => (
@@ -96,7 +184,66 @@ class Animal extends Component {
     );
   }
   pastClick = () => {
-    alert("Test");
+    if (this.state.timeSelectTodayClasses.includes("time-select-active")) {
+      this.setState({
+        timeSelectTodayClasses: "time-select-middle-btn",
+      });
+    }
+
+    if (this.state.timeSelectFutureClasses.includes("time-select-active")) {
+      this.setState({
+        timeSelectFutureClasses: "time-select-right-btn",
+      });
+    }
+
+    this.setState({
+      timeSelectPastClasses:
+        this.state.timeSelectPastClasses + " time-select-active",
+    });
+    this.resetState();
+    this.getEvents("past");
+  };
+
+  todayClick = () => {
+    if (this.state.timeSelectPastClasses.includes("time-select-active")) {
+      this.setState({
+        timeSelectPastClasses: "time-select-left-btn",
+      });
+    }
+
+    if (this.state.timeSelectFutureClasses.includes("time-select-active")) {
+      this.setState({
+        timeSelectFutureClasses: "time-select-right-btn",
+      });
+    }
+
+    this.setState({
+      timeSelectTodayClasses:
+        this.state.timeSelectTodayClasses + " time-select-active",
+    });
+    this.resetState();
+    this.getEvents("today");
+  };
+
+  futureClick = () => {
+    if (this.state.timeSelectPastClasses.includes("time-select-active")) {
+      this.setState({
+        timeSelectPastClasses: "time-select-left-btn",
+      });
+    }
+
+    if (this.state.timeSelectTodayClasses.includes("time-select-active")) {
+      this.setState({
+        timeSelectTodayClasses: "time-select-middle-btn",
+      });
+    }
+
+    this.setState({
+      timeSelectFutureClasses:
+        this.state.timeSelectFutureClasses + " time-select-active",
+    });
+    this.resetState();
+    this.getEvents("future");
   };
 }
 
