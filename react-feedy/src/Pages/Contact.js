9511ca2ec118a2ba2fa71929a33d8c600f4d67fa
duartemelo <duarteribeirodemelo@hotmail.com>
2020-04-24 17:34:18 +0100

    Utilizador consegue ver informações de contacto

--- react-feedy/src/Pages/Contact.js
+++ react-feedy/src/Pages/Contact.js
@@ -3,6 +3,34 @@ import "../styles/global/Main.css";
 import app from "../firebase";
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vetContactInfo: [],
+    };
+  }
+
+  componentDidMount() {
+    let currentComponent = this;
+    setTimeout(function () {
+      currentComponent.getVetContactInfo();
+    }, 10);
+  }
+
+  getVetContactInfo() {
+    const db = app.database();
+    const ref = db.ref("vet-contact-info");
+    let currentComponent = this;
+    ref.on("child_added", function (snapshot) {
+      var infoObtained = snapshot.val();
+      currentComponent.setState({
+        vetContactInfo: currentComponent.state.vetContactInfo.concat(
+          infoObtained
+        ),
+      });
+    });
+  }
+
   render() {
     return (
       <div>
@@ -15,6 +43,8 @@ class Contact extends Component {
         </button>
         <h1>Contact Page Test</h1>
         <p>This is a test page</p>
+        <p>Vet Contact Info below</p>
+        <p>{this.state.vetContactInfo}</p>
       </div>
     );
   }
